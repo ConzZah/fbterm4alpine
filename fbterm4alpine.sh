7d4558654f157a7929f33e0fca4d0304d9d98687
ConzZah <69615452+ConzZah@users.noreply.github.com>
2026-07-03 02:29:17 +0200

    qol

--- fbterm4alpine.sh
+++ fbterm4alpine.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
-### /// fbterm4alpine.sh // ConzZah // 2026-07-02 04:42 ///
+### /// fbterm4alpine.sh // ConzZah // 2026-07-03 00:32 ///
 
 ## this script will build fbterm for alpine (we're linking statically ofc)
 
-apk add build-base pkgconf gpm-dev expat-static zlib-static bzip2-static brotli-static libpng-static freetype-static fontconfig-static linux-headers git
+doso=""
+## if we're not root, fill $doso
+[ "$(id -u)" = "1000" ] && doso="doas"
+
+$doso apk add build-base pkgconf gpm-dev expat-static zlib-static bzip2-static brotli-static libpng-static freetype-static fontconfig-static ncurses linux-headers git || exit 1
 
 git clone https://github.com/sfzhi/fbterm
 
@@ -12,10 +16,8 @@ cd fbterm || exit 1
 
 make clean
 
-ldflags="$(pkg-config --static --libs fontconfig freetype2 expat libpng bzip2)"
+sh configure
 
-./configure LDFLAGS="-static $ldflags"
+make LDFLAGS="-static" LIBS="$(pkg-config --static --libs fontconfig freetype2 expat libpng bzip2)"
 
-make LDFLAGS="-static $ldflags" LIBS="$ldflags"
-
-make install
+$doso make install
